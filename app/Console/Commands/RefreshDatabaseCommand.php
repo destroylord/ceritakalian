1df7e948044135c1d064701270fecaf0e7b67e21
destroylord <masapin68@gmail.com>
2020-08-03 17:07:09 +0700

    menggunakan custom command, faktory, dan seed

---
+++ app/Console/Commands/RefreshDatabaseCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshDatabaseCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:segarkan';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Menyegarkan database yang kita punya';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // action
+        // di migrate baru di seed / migrate:refresh --seed sama saja seperti itu
+        $this->call('migrate:refresh');
+
+        $this->call('db:seed');
+
+        $this->info('Semua database telah siap digunakan');
+    }
+}
---
+++ database/seeds/StoriesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Story;
+
+class StoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Story::class, 50)->create();
+    }
+}
